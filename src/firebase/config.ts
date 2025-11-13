a3a832923561c08eb14535327da5b2e4899e0639
Fran Parra <franciscoparra22@gmail.com>
2025-11-13 03:53:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1130931899-95a52",
+  "appId": "1:352869854567:web:8a80b70a5e00557cb1d3c2",
+  "apiKey": "AIzaSyD3i_6e73gF4YILwiZoVa_IBzOjddxtrKs",
+  "authDomain": "studio-1130931899-95a52.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "352869854567"
+};
